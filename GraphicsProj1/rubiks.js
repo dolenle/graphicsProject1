49e4092336061facddc0fb110fcaee83e6954efb
Dolen Le <le2@cooper.edu>
2015-04-10 21:08:42 -0400

    almost done\!

--- GraphicsProj1/rubiks.js
+++ GraphicsProj1/rubiks.js
@@ -35,16 +35,14 @@ var eye;
 const at = vec3(0.0, 0.0, 0.0);
 const up = vec3(0.0, 1.0, 0.0);
 
-//-----------NEW STUFF-------------------------------------------------------------------------------------
-
-var spacing = 0.5;
+var spacing = 0.42;
 var rubiks = [];
 var animationQueue = [];
 var animSpeed = 3;
 
-var camDirec = [0, 0, 0];
-var camAccel = 1.05;
-var camSpeed = 0.5;
+var camAccel = [1,1];
+var camSpeed = [0,0];
+var rot = [15,20];
 
 var rows = [[],[],[]]; //rows of the rubiks cube
 var cols = [[],[],[]]; //columns of the cube
@@ -54,7 +52,7 @@ var planes = [[[],[],[]], [[],[],[]], [[],[],[]]];
 var animRunning = 0;
 
 var cubeColors = [
-	[ 0.2, 0.2, 0.2, 0.5 ], //black
+	[ 0.2, 0.2, 0.2, 1.0 ], //black
 	[ 1.0, 0.0, 0.0, 1.0 ], //red
 	[ 1.0, 1.0, 0.0, 1.0 ], //yellow
 	[ 0.0, 0.6, 0.0, 1.0 ], //green
@@ -74,8 +72,30 @@ var Colors = {
 };
 
 var axis=0;
-var rot = [0,30,0];
+
 var tempMV = mat4().create;
+
+function checkSolve() {
+	var sideColor;
+	for (var i in planes) { //check
+		for (var j in planes[i]) {
+			if(j==0) {
+				sideColor = rubiks[planes[i][j][0]].nextColors[5-i];
+				for(var k in planes[i][j])
+					if(rubiks[planes[i][j][k]].nextColors[5-i]!=sideColor) {
+						return false;
+					}
+			} else if(j==2) {
+				sideColor = rubiks[planes[i][j][0]].nextColors[i];
+				for(var k in planes[i][j])
+					if(rubiks[planes[i][j][k]].nextColors[i]!=sideColor) {
+						return false;
+					}
+			}
+		}
+	}
+	return true;
+}
 
 function rubiksCube() {
 	var index=0;
@@ -90,7 +110,9 @@ function rubiksCube() {
 		for(var y=0; y<3; y++) {
 			for(var z=0; z<3; z++) {
 				rubiks.push(new Cube([(x-1)*spacing, (y-1)*spacing, (z-1)*spacing, 1.0], [x, y, z],
-					[rightColors[x], topColors[y], frontColors[z], backColors[z], bottomColors[y], leftColors[x]]));
+					[rightColors[x], topColors[y], frontColors[z],
+					backColors[z], bottomColors[y], leftColors[x]]
+				));
 				planes[0][x].push(index);
 				planes[1][y].push(index);
 				planes[2][z].push(index);
@@ -98,10 +120,10 @@ function rubiksCube() {
 			}
 		}
 	}
-	//evaluatePlanes();
 	console.log("[rubiksCube]");
 }
 
+//Construct a cube
 function Cube(position, planes, colors) {
 	this.origin = position;
 	this.curPos = planes;
@@ -115,6 +137,7 @@ function Cube(position, planes, colors) {
 	this.isAnimating = 0;
 }
 
+//Draw a cube
 Cube.prototype.render = function(x, y, z) {
 	tempMV = modelViewMatrix;  //backup modelViewMatrix
 	// Move to cube frame
@@ -143,22 +166,20 @@ Cube.prototype.render = function(x, y, z) {
 	modelViewMatrix = tempMV;
 }
 
-var effectiveFPMS = 60 / 1000;
+//Cube animation step
 Cube.prototype.animate = function(elapsedTime) {
-	var animAngle = animSpeed * Math.round(effectiveFPMS * elapsedTime);
+	var animAngle = animSpeed * Math.round(0.06 * elapsedTime);
 	//for(var i in this.target) {
 	if(this.isAnimating == 1) {
 		if(this.target > 1) {
 			this.angle[this.axis] += animAngle * this.direction;
 			this.target -= animAngle;
 		} else {
-			//this.target = 0;
 			this.isAnimating = 0;
 			this.angle = [0, 0, 0];
 			this.direction=0;
 			animRunning = 0;
 			this.colors = this.nextColors.slice(0);
-			//console.log(this.colors);
 		}
 	}
 	//}
@@ -167,13 +188,16 @@ Cube.prototype.animate = function(elapsedTime) {
 var lastTime = 0;
 
 //plane: 0=column, 1=row, 2=face
-function makeTurn(p, i, d) { //plane, index, direction
+function makeTurn(data) { //plane, index, direction
+	var p = data[0];
+	var i = data[1];
+	var d = data[2];
 	if(!animRunning) {
 		var p1 = (p+1)%3; //other plane 1
 		var p2 = (p+2)%3; //other plane 2
 		
-		if(p%2 != 0) {
-				p2 = [p1, p1 = p2][0];
+		if(p%2 != 0) { //swap p1/p2 for y-axis
+			p2 = [p1, p1 = p2][0];
 		}
 
 		for(var x in planes[p][i]) {
@@ -229,9 +253,55 @@ function makeTurn(p, i, d) { //plane, index, direction
 			animRunning = 1;
 		}
 	}
+	if(checkSolve()) {
+		$("#notify").slideDown();
+	} else {
+		$("#notify").fadeOut();
+	}
 }
 
-//-----------END NEW STUFF--------------------------------------------------------------------------------------------------
+function getRelativePlane(p, i, d) {
+	return [p, i, d];
+//NOT COMPLETE
+// 	var newPlanes = [0,1,2];
+// 	var newIndex = i;
+// 	var newDirec = d;
+// 	var angleY = Math.abs(rot[1])%360;
+// 	if(rot[0] > 45) { //facing top
+// 		//console.log("bar");
+// 		if(rot[1]>0 && angleY>45 && angleY<135 || rot[1]<0 && angleY>225 && angleY<315) {
+// 			newPlanes = [2,0,1]
+// 			if(p==0)
+// 				newDirec = -d;
+// 		}
+// 		if(angleY%180 > 45 && angleY%180 < 135) {
+// 			newPlanes = [2,0,1]
+// 			console.log("bar1");
+// 			//return [p,i,d];
+// 		} else {
+// 			console.log("bar2");
+// 			newPlanes = [0,2,1];
+// 			//return [p,i,d];
+// 		}
+// 		if(rot[1]<0 && angleY<135 || rot[1]>0 && angleY<45) {
+// 			console.log("negate!");
+// 			newIndex = (-i+2);
+// 			newDirec = -d;
+// 		}		
+// 	} else if(rot[0] < -45) { //facing bottom
+// 		//console.log("foo");
+// 	} else if(angleY%180 > 45 && angleY%180 < 135) { //facing sides
+// 		newPlanes = [2,1,0];
+// 		if(p!=0 && (rot[1]>0 && angleY>45 && angleY<135 || rot[1]<0 && angleY>225 && angleY<315)) {
+// 			newIndex = (-i+2);
+// 			newDirec = -d; //reverse spin if flipped
+// 		}
+// 	} else if(angleY<180 && angleY%90 > 45 || angleY>180 && angleY%180 < 45) {
+// 		newIndex = (-i+2);
+// 		newDirec = -d;
+// 	}
+// 	return [newPlanes[p],newIndex,newDirec];
+}
 
 function quad(a, b, c, d) {
 	pointsArray.push(vertices[a]);
@@ -287,7 +357,6 @@ window.onload = function init() {
     cBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
     gl.bufferData( gl.ARRAY_BUFFER, flatten(colorsArray), gl.DYNAMIC_DRAW );
-    //gl.bufferData(gl.ARRAY_BUFFER, 15552, gl.STATIC_DRAW );
     
     vColor = gl.getAttribLocation( program, "vColor" );
     gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
@@ -310,6 +379,7 @@ window.onload = function init() {
 
 function setupUI() {
 	//event handlers
+	$('#accordion').accordion();
 	document.getElementById("zFarSlider").onchange = function(event) {
         far = event.target.value;
     };
@@ -331,36 +401,189 @@ function setupUI() {
     document.getElementById("fovSlider").onchange = function(event) {
         fovy = event.target.value;
     };
-    
     document.getElementById( "resetRot" ).onclick = function () {
-		camDirec =  [0,0,0];
 		rot = [0,0,0];
-		camSpeed = 0.05;
+		camSpeed = [0,0,0];
     };
-    document.getElementById( "tButton" ).onclick = function () {
-		animationQueue.push([0, document.getElementById( "ind" ).value, document.getElementById( "dir" ).value]);
+    document.getElementById("tButton").onclick = function () {
+		animationQueue.push([0, document.getElementById( "ind" ).value,
+		document.getElementById( "dir" ).value]);
     };
-    document.getElementById( "tButton2" ).onclick = function () {
-		animationQueue.push([1, document.getElementById( "ind" ).value, document.getElementById( "dir" ).value]);
+    document.getElementById("tButton2").onclick = function () {
+		animationQueue.push([1, document.getElementById( "ind" ).value,
+		document.getElementById( "dir" ).value]);
     };
-    document.getElementById( "tButton3" ).onclick = function () {
-		animationQueue.push([2, document.getElementById( "ind" ).value, document.getElementById( "dir" ).value]);
+    document.getElementById("tButton3").onclick = function () {
+		animationQueue.push([2, document.getElementById( "ind" ).value,
+		document.getElementById( "dir" ).value]);
     };
+    
+    $("#hide_notify").click(function() {
+    	$("#notify").slideUp();
+    });
+    
+    $("#hide_error").click(function() {
+    	$("#error_msg").slideUp();
+    });
+    
+     $("#gen_rand").click(function() {
+     	var cmds = ["F","B","U","D","L","R","f","b","u","d","l","r"];
+     	var end = $("#rand_num").val();
+     	if($.isNumeric(end)) {
+			$("#rand_txt").val(""); //clear text
+			for(var i=0; i < end; i++) {
+				$("#rand_txt").val($("#rand_txt").val()+cmds[Math.round(11*Math.random())]);
+				if(Math.round(Math.random())) {
+					$("#rand_txt").val($("#rand_txt").val()+"'");
+				}
+			}
+		} else {
+			$("#error_msg").slideDown();
+		}
+    });
+    
+    $("#run_rand").click(function() {
+    	$("#cmd_txt").val(""); //clear commands
+    	$("#cmd_txt").val($("#rand_txt").val()); //copy random commands
+    	$('#accordion').accordion({active: 0});
+    	runCommands();
+    });
+    
+    $("#run_cmd").click(function() {
+    	runCommands();
+    });
+    
+    $("#saveButton").click(function() {
+		var data = [];
+		for (var i in rubiks) {
+			data.push(rubiks[i].colors);
+		}
+		saveAs(new Blob([JSON.stringify(data)], {type: "text/plain;"}), ($("#file_name").val()+".txt" || "cube.txt"));
+    });
+    
+	$("#load_save").on("change", function() {
+		var reader = new FileReader();
+		reader.onload = function() {
+			var data 
+			try {
+				data = JSON.parse(reader.result);
+				console.log(data.length);
+				if(data.length == 27) {
+					var id = 0;
+					for(var i in data) {
+						if(data[i].length==6) {
+							rubiks[id].colors=data[i];
+						} else {
+							$("#error_msg").slideDown();
+							return;
+						}
+					id++;
+					}
+				} else {
+					$("#error_msg").slideDown();
+					return;
+				}
+			} catch(e) {
+				$("#error_msg").slideDown();
+				return;
+			}
+		}
+		if(!this.files[0]) {
+			$("#error_msg").slideDown();
+		} else {
+			reader.readAsText(this.files[0]);
+		}
+	});
 	
 	document.onkeydown = function(event) {
-		if(event.keyCode > 36 && event.keyCode < 41) {
-			
-			var key = (event.keyCode-38)%2
-		if(key==0)
-			camDirec[Math.abs(key)]+=camSpeed*=camAccel*(event.keyCode-39);
-		else
-			camDirec[Math.abs(key)]+=(camSpeed*=camAccel)*key;
+		if(event.keyCode > 36 && event.keyCode < 41 && $(document.activeElement).prop("tagName")=="BODY") { //arrow keys
+			event.preventDefault(); //prevent scrolling
+			var key = (event.keyCode-38)%2;
+			var id = Math.abs(key);
+			var reverseX=1;
+			var angle = Math.abs(rot[0]%360);
+			if(angle>90 && angle<270) { //reverse spin if flipped
+				reverseX=-1;
+			}
+			if(Math.abs(camSpeed[id]) < 1) {
+				camSpeed[id] = 1;
+			}
+			if(key==0) {
+				camAccel[id]=(1.1*(event.keyCode-39));
+			} else {
+				camAccel[id]=(1.1*key*reverseX);
+			}
 		}
 	}
 	document.onkeyup = function() {
-		camSpeed=0.5;
-		//camAccel = 0.2;
-		//console.log(camSpeed);
+		if(event.keyCode > 36 && event.keyCode < 41) { //arrow keys
+			var key = (event.keyCode-38)%2;
+			var id = Math.abs(key);
+			if(key==0) {
+				camAccel[id]=0.9*sign(camAccel[id]);
+			} else {
+				camAccel[id]=0.9*sign(camAccel[id]);
+			}
+		}
+	}
+}
+
+function sign(x) { return (x >> 31) + (x > 0 ? 1 : 0); }
+
+function runCommands() {
+	//get commands text and remove smart quotes
+	if(!$("#cmd_txt").val()) {
+		$("#error_msg").slideDown();
+	} else {
+		$("#error_msg").slideUp();
+		var txt=$("#cmd_txt").val().replace( /\u2018|\u2019|\u201A|\uFFFD/g, "'" );
+		var regex = /[FBUDLRfbudlr]'?/g;
+		var commands=txt.match(regex).reverse();
+		for(var i in commands) {
+			var cmd = commands[i];
+			var d = 1;
+			if(cmd.length==2) {
+				d=-1;
+			}
+			switch(cmd[0]) {
+				case "F":
+					animationQueue.push([2, 2, -d]);
+					break;
+				case "B":
+					animationQueue.push([2, 0, d]);
+					break;
+				case "U":
+					animationQueue.push([1, 2, -d]);
+					break;
+				case "D":
+					animationQueue.push([1, 0, d]);
+					break;
+				case "L":
+					animationQueue.push([0, 0, d]);
+					break;
+				case "R":
+					animationQueue.push([0, 2, -d]);
+					break;
+				case "f":
+					animationQueue.push([2, 1, -d]);
+					break;
+				case "b":
+					animationQueue.push([2, 1, d]);
+					break;
+				case "u":
+					animationQueue.push([1, 1, -d]);
+					break;
+				case "d":
+					animationQueue.push([1, 1, d]);
+					break;
+				case "l":
+					animationQueue.push([0, 1, -d]);
+					break;
+				case "r":
+					animationQueue.push([0, 1, d]);
+					break;
+			}
+		}
 	}
 }
 
@@ -372,12 +595,12 @@ var render = function() {
     
     modelViewMatrix = lookAt(eye, at , up);
     
-    rot[1]+=camDirec[1];
-	if((rot[0]<90 || camDirec[0]<0) && (rot[0]>-90 || camDirec[0]>0))
-		rot[0]+=camDirec[0];
-	else {
-		camDirec[0]=0;
+	if(rot[0]>89 && camAccel[0]>0 || rot[0]<-89 && camAccel[0]<0) {
+		camSpeed[0]=0;
 	}
+    
+    rot[0]+=(camSpeed[0]*=sign(camSpeed[0])*camAccel[0]);
+    rot[1]+=(camSpeed[1]*=sign(camSpeed[1])*camAccel[1]);
     modelViewMatrix = mult(modelViewMatrix, rotate(rot[0], [1, 0, 0] ));
     modelViewMatrix = mult(modelViewMatrix, rotate(rot[1], [0, 1, 0] ));
     //modelViewMatrix = mult(modelViewMatrix, rotate(rot[2], [0, 0, 1] ));
@@ -391,7 +614,7 @@ var render = function() {
     //Perform Animations
     if(!animRunning && animationQueue.length) {
 		var ani = animationQueue.pop();
-		makeTurn(ani[0], ani[1], ani[2]);
+		makeTurn(getRelativePlane(ani[0], ani[1], ani[2]));
 	}
     var timeNow = new Date().getTime();
 	if (lastTime != 0) {
